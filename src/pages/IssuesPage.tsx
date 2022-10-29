0eff2466fd1bb7f8cd1f7e6cc189f134d16d36a5
Chaedie <chaedong.im.dev@gmail.com>
2022-10-30 03:16:23 +0900

    Feat: 이슈페이지 구현
    useFetch 구현

--- src/pages/IssuesPage.tsx
+++ src/pages/IssuesPage.tsx
@@ -1,9 +1,43 @@
+import React, { useState } from 'react';
+import { IssuesService } from '../apis/IssuesService';
+import useFetch from '../hooks/useFetch';
+
+interface Issue {
+  number: number;
+  title: string;
+  user: IssueUser;
+  created_at: string;
+  comments: number;
+  body: string;
+}
+
+interface IssueUser {
+  login: string;
+  avatar_url: string;
+}
 function IssuesPage() {
+  const [issueList, setIssueList] = useState<Issue[]>();
+
+  const { isLoading, errors } = useFetch(setIssueList, IssuesService.getIssues);
+  if (errors)
+    return <p style={{ color: 'red' }}>got Error... refresh (cmd + r / f5)</p>;
+  if (isLoading) return <p>loading...</p>;
 
   return (
-    <>
-      <h1>IssuesPage</h1>
-    </>
+    <ul>
+      {issueList &&
+        issueList.map(({ number, title, user, created_at, comments, body }) => (
+          <li key={number}>
+            <p>
+              #{number} title: {title}
+            </p>
+            <p>
+              <span>작성자: {user.login}</span>
+              <span>작성일: {created_at}</span> <span>코멘츠: {comments}</span>
+            </p>
+          </li>
+        ))}
+    </ul>
   );
 }
 

a53e9168437e1400cd7a705d80d98d696a1edd98
idjaeha <jaehanim1@gmail.com>
2022-10-30 18:01:00 +0900

    Feat: 로딩 중 구현 실패로, 더 이상 로딩이 불가능하면 사라지게 변경

--- src/components/IssueLoader.tsx
+++ src/components/IssueLoader.tsx
@@ -1,12 +1,14 @@
 // @flow
 import * as React from 'react';
 import { useCallback, useEffect, useRef } from 'react';
+import styled from 'styled-components';
 import { useLoadIssues } from '../hooks/useLoadIssues';
 
 export function IssueLoader() {
   const loadIssues = useLoadIssues();
   const observerRef = useRef<IntersectionObserver | null>(null);
   const divRef = useRef<HTMLDivElement>(null);
+  const [isEnd, setIsEnd] = React.useState(false);
 
   const onIntersect: IntersectionObserverCallback = useCallback(
     async ([entry], observer) => {
@@ -14,6 +16,7 @@ export function IssueLoader() {
         observer.unobserve(entry.target);
         const isEnd = await loadIssues();
         if (isEnd === false) observer.observe(entry.target);
+        setIsEnd(isEnd);
       }
     },
     [loadIssues]
@@ -29,5 +32,22 @@ export function IssueLoader() {
     }
   }, [onIntersect]);
 
-  return <div ref={divRef}>불러오기</div>;
+  if (isEnd) return null;
+  return <S.Loading ref={divRef}>데이터를 불러오는 중입니다.</S.Loading>;
 }
+
+const S = {
+  Loading: styled.div`
+    & {
+      margin: 1rem;
+      padding: 0.5rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 2rem;
+      border-radius: 0.5rem;
+      background-color: grey;
+      color: white;
+    }
+  `,
+};
